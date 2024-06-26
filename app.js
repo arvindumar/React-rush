6e017825560c762f0ee6b9369ac6c79fe48a0d81
arvindumar <arvindumar4@gmail.com>
2024-06-26 23:12:16 +0530

    Fixing_app_js_file

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
-// const heading = React.createElement("h1", {id: "heading"}, "Jai Ho!");
-//     const root = ReactDOM.createRoot(document.getElementById("root"));
-//   const para = React.createElement("p", {}, "Lorem10");
-// //   root.render(para);
-// // root.appendChild(para);
-// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading1 = React.createElement("h1",{},"i'm h1 of child1");
+const heading2 = React.createElement("h2",{},"i'm h2 of child1");
+const heading3 = React.createElement("h1",{},"i'm h1 of child2");
+const heading4 = React.createElement("h2",{},"i'm h2 of child2");
+const child1 = React.createElement("div",{id: "child1"},[heading1,heading2]);
+const child2 = React.createElement("div",{id: "child2"},[heading3,heading4]);
+const parent = React.createElement("div",{id: "parent"},[child1,child2]);
 
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm a h1 tag!</h1>
- *      </div>
- * </div>
- *
- */
-// CREATE THE NESTED STRUCTURE LIKE ABOVE
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "I'm a h1 tag")
-//   )
-// );
-// TO CREATE THE SIBLINGS PUT REACT.create in array
-/**
- * <div id="parent">
- *      <h1>Name is H1 tag</h1>
- *      <h2>Name is h2 tag</h2>
- * </div>
- *
- */
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("H1", {}, "Name is h1 Tag"),
-  React.createElement("h2", {}, "Name is h2 tag"),
-]);
-//OR we do it like this
-// const heading1=React.createElement("H1", {}, "Name is h1 Tag");
-// const heading2=React.createElement("h2", {}, "Name is h2 tag");
-// const parent = React.createElement("div", { id: "parent" }, [heading1,heading2]); 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
+// what are npm packages
